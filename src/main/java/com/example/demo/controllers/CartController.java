8d3fd9ef0465fba4ec95a5d4d35b88c56a74349a
unknown <ayman.chabchoub.2018@gmail.com>
2023-04-06 03:49:36 +0200

    message

---
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -0,0 +1,108 @@
+package com.example.demo.controllers;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Cart;
+import com.example.demo.models.CartItem;
+import com.example.demo.services.CartService;
+import com.example.demo.services.UserDetailsImpl;
+
+@RestController
+@RequestMapping("/api/auth")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/cart")
+    public ResponseEntity<?> addToCart(@RequestParam Long productId, @RequestParam int quantity, @RequestParam Long userId) {
+        cartService.addProductToCart(userId, productId, quantity);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/cart/delete")
+    public ResponseEntity<?> removeFromCart(@RequestParam Long productId, @RequestParam int quantity) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) auth.getPrincipal();
+        Long userId = ((UserDetailsImpl) userDetailsService.loadUserByUsername(userDetails.getUsername())).getId();
+        cartService.removeProductFromCart(userId, productId, quantity);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping("/cart")
+    public ResponseEntity<List<CartItem>> getCart(@RequestParam Long userId) {
+        try {
+            Cart cart = cartService.getCartByUserId(userId);
+            if (cart == null) {
+                return ResponseEntity.notFound().build();
+            }
+            List<CartItem> cartItems = new ArrayList<>();
+            for (CartItem cartItem : cart.getCartItems()) {
+                cartItems.add(cartItem);
+            }
+            return ResponseEntity.ok().body(cartItems);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("cart/user/{userId}")
+    public long getCartIdByUserId(@PathVariable Long userId) {
+        return cartService.getCartIdByUserId(userId);
+    }
+
+    @GetMapping("/cart/content")
+    public double totale(@RequestParam Long userId)
+    {	System.out.println("jksgjdgjsdgfjshqgf");
+    	return cartService.getTotal(userId);
+    }
+    @GetMapping("/cart/items")
+    public ResponseEntity<List<CartItem>> getCartItemsByCartId(@RequestParam Long cartId) {
+        try {
+            Optional<Cart> optionalCart = cartService.getCartById(cartId);
+            if (optionalCart.isPresent()) {
+                Cart cart = optionalCart.get();
+                List<CartItem> cartItems = cartService.getCartItemsByCart(cart);
+                return ResponseEntity.ok().body(cartItems);
+            } else {
+                throw new RuntimeException("Cart not found for id :: " + cartId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @CrossOrigin(origins = "http://localhost:4200")
+
+    @GetMapping("/cart/cartid")
+    public Long getCartIdByCartItem(@RequestParam Long cartItemId) {
+        return cartService.getCartIdByCartItem(cartItemId);
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/demo/services/CartService.java
@@ -0,0 +1,124 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.models.Cart;
+import com.example.demo.models.CartItem;
+import com.example.demo.models.Produit;
+import com.example.demo.models.User;
+import com.example.demo.repository.CartRepository;
+import com.example.demo.repository.ProduitRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.repository.cartItemRepositry;
+
+@Service
+public class CartService {
+    private static final Logger logger = LoggerFactory.getLogger(CartService.class);
+
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private cartItemRepositry cartItemRepositry;
+    
+    @Autowired
+    private ProduitRepository produitRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+    public Optional<Cart> getCartById(Long cartId) {
+        logger.info("Retrieving cart with ID {}", cartId);
+        return cartRepository.findById(cartId);
+    }
+    
+    public List<CartItem> getCartItemsByCart(Cart cart) {
+        logger.info("Retrieving cart items for cart with ID {}", cart.getId());
+        return cart.getCartItems();
+    }
+    public Long getCartIdByCartItem(Long cartItemId) {
+        Optional<CartItem> optionalCartItem = cartItemRepositry.findById(cartItemId);
+        if (optionalCartItem.isPresent()) {
+            return optionalCartItem.get().getCart().getId();
+        } else {
+            throw new IllegalArgumentException("Invalid cart item id: " + cartItemId);
+        }
+    }
+
+
+    public Cart getCartByUserId(Long userId) {
+        logger.info("Retrieving cart for user with ID {}", userId);
+        return cartRepository.findByUserId(userId).orElse(null);
+    }
+    public Long getCartIdByUserId(Long userId) {
+        Optional<Cart> cart = cartRepository.findByUserId(userId);
+        return cart.map(Cart::getId).orElse(null);
+    }
+
+
+    public void addProductToCart(Long userId, Long productId, int quantity) {
+        logger.info("Adding product with ID {} and quantity {} to cart for user with ID {}", productId, quantity, userId);
+        Cart cart = getCartByUserId(userId);
+        if (cart == null) {
+            logger.info("Creating new cart for user with ID {}", userId);
+            cart = new Cart();
+            User user = userRepository.findById(userId).orElse(null);
+            if (user == null) {
+                logger.warn("User with ID {} not found", userId);
+                return;
+            }
+            cart.setUser(user);
+        }
+        Produit product = produitRepository.findById(productId).orElse(null);
+        if (product != null) {
+            CartItem cartItem = cart.findCartItemByProduitId(productId);
+            if (cartItem != null) {
+                cartItem.setQuantity(cartItem.getQuantity() + quantity);
+            } else {
+                cart.addCartItem(new CartItem(product, quantity));
+            }
+            cartRepository.save(cart);
+            logger.info("Product with ID {} added to cart for user with ID {}", productId, userId);
+        } else {
+            logger.warn("Product with ID {} not found", productId);
+        }
+    }
+
+    public void removeProductFromCart(Long userId, Long productId, int quantity) {
+        logger.info("Removing product with ID {} and quantity {} from cart for user with ID {}", productId, quantity, userId);
+        Cart cart = getCartByUserId(userId);
+        if (cart == null) {
+            logger.warn("Cart not found for user with ID {}", userId);
+            return;
+        }
+        CartItem cartItem = cart.findCartItemByProduitId(productId);
+        if (cartItem != null) {
+            if (cartItem.getQuantity() > quantity) {
+                cartItem.setQuantity(cartItem.getQuantity() - quantity);
+            } else {
+                cart.removeCartItem(cartItem);
+            }
+            cartRepository.save(cart);
+            logger.info("Product with ID {} removed from cart for user with ID {}", productId, userId);
+        } else {
+            logger.warn("Product with ID {} not found in cart for user with ID {}", productId, userId);
+        }
+    }
+
+    public double getTotal(Long userId) {
+        logger.info("Calculating total for cart of user with ID {}", userId);
+        Cart cart = getCartByUserId(userId);
+        if (cart == null) {
+            logger.warn("Cart not found for user with ID {}", userId);
+            return 0;
+        }
+        return cart.getTotal();
+    }
+    
+
+    
+}
